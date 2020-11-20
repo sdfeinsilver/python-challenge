7f0a0483e43806e1f315608681072b4fbe045dd9
Simon Feinsilver <sdfeinsilver@gmail.com>
2020-11-20 13:26:21 -0600

    Finished net amount

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,9 +9,7 @@ import csv
 
 #Variables
 months = 0
-
-
-
+net_amount = 0
 
 #create path to csv
 csvpybank = os.path.join("Resources", "budget_data.csv")
@@ -20,6 +18,17 @@ csvpybank = os.path.join("Resources", "budget_data.csv")
 with open(csvpybank) as csvfile:
     csvreader = csv.reader(csvfile, delimiter=",")
     header = next(csvreader)
+    
+    #After the header, read each row    
     for row in csvreader:
+
+        #Total number of months        
         months = months + 1
+
+        #Net Amount of Profits/Losses
+        net_amount = net_amount + int(row[1])
+
+
+#Print your Data
 print(f"Total Months: {months}")
+print(f"Net Amount: {net_amount}")
